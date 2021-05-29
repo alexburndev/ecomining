c48be526d89a4d6cdcdc5dd514a463b556174f9a
alexburndev <62510405+alexburndev@users.noreply.github.com>
2021-05-29 16:31:17 +0300

    Update greentoken.sol

--- greentoken.sol
+++ greentoken.sol
@@ -1,13 +1,16 @@
-pragma solidity ^0.5.8;
+pragma solidity ^0.6.0;
 
 /*
-Green Token is a part of the unified Ecomining system.
-Mining of various coins is developing more and more in the world. 
+Green Token is part of the unified Ecomining system.
+Mining of various coins is developing more and more in the world.
 A huge amount of electricity is wasted.
-Our Team is developing a completely new mining concept - Green mining.
-Using the Force of Nature given to us, we can mine (extract) everything: 
-water, green spaces, oxygen - everything that Mother Nature produces for us.
+Our team is developing a completely new mining concept - Green Mining.
+Using the Force of Nature given to us, we can extract (extract) everything:
+water, green spaces, oxygen - everything that Mother Nature gives us.
 At the same time, we do not destroy, but restore Nature - and this is our Power!
+By purchasing our coin, you are participating in the noblest Mission on the 
+planet - the transformation of our world with you, our House, our Planet.
+All funds will be directed to environmental programs.
 
 github https://github.com/alexburndev/ecomining/blob/main/greentoken.sol
 */
@@ -131,47 +134,117 @@ library SafeMath {
     }
 }
 
+contract TRC20Standard {
+	using SafeMath for uint256;
+//	uint public totalSupply;
+	
+//	string public name;
+//	uint8 public decimals;
+//	string public symbol;
+//	string public version;
+	
+	mapping (address => uint256) balances;
+	mapping (address => mapping (address => uint)) allowed;
 
-contract owned {
-    address public owner;
+	//Fix for short address attack against ERC20
+	modifier onlyPayloadSize(uint size) {
+		assert(msg.data.length == size + 4);
+		_;
+	} 
+
+//	function balanceOf(address _owner) public view returns (uint balance) {
+//		return balances[_owner];
+//	}
+
+/*
+	function transfer(address _recipient, uint _value) public onlyPayloadSize(2*32) {
+	    require(balances[msg.sender] >= _value && _value > 0);
+	    balances[msg.sender] = balances[msg.sender].sub(_value);
+	    balances[_recipient] = balances[_recipient].add(_value);
+	    emit Transfer(msg.sender, _recipient, _value);        
+        }
+*/
+
+	function transferFrom(address _from, address _to, uint _value) public {
+	    require(balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0);
+            balances[_to] = balances[_to].add(_value);
+            balances[_from] = balances[_from].sub(_value);
+            allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+            emit Transfer(_from, _to, _value);
+        }
+
+	function  approve(address _spender, uint _value) public {
+		allowed[msg.sender][_spender] = _value;
+		emit Approval(msg.sender, _spender, _value);
+	}
+
+	function allowance(address _spender, address _owner) public view returns (uint balance) {
+		return allowed[_owner][_spender];
+	}
+
+	//Event which is triggered to log all transfers to this contract's event log
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint _value
+		);
+		
+	//Event which is triggered whenever an owner approves a new allowance for a spender.
+	event Approval(
+		address indexed _owner,
+		address indexed _spender,
+		uint _value
+		);
+}
+
+contract owned is TRC20Standard {
     
-    function Ownabler() public {
-    owner = msg.sender;
+    address payable public owner = 0x4C3D19dedc22E98EA8e2fC753DA423740415A326; 
+    
+   // Token TRC20 GT = Token TRC20(TGc4179QHaJxEau8GN65SneJPH1FxTAwV4);
+
+    function changeOwner(address payable _newOwner) public onlyOwner {
+        owner = _newOwner;
     }
- 
     
     modifier onlyOwner {
         require(owner == msg.sender);
         _;
     }
     
-    //  Внимание!!! Для TRON переделать на Tron - адрес
-    address public addressSupportProject = 0x009AE8DDCBF8aba5b04d49d034146A6b8E3a8B0a; //AB
+    address payable public addressSupportProject = 0xf431cDDa426b7CA76FA1c19FDd1AF0ef40226259;
     
-       function setNewaddressSupportProject(address _addr ) public onlyOwner {
+       function setNewaddressSupportProject(address payable _addr ) public onlyOwner {
        require (_addr != address(0));
        addressSupportProject = _addr;
     }
     
+    address payable public saleAgent;
     
+    function setSaleAgent(address payable _newSaleAgnet) public {
+    require(msg.sender == saleAgent || msg.sender == owner);
+    saleAgent = _newSaleAgnet;
+  }
 }
 
 
 
 contract Working is owned {
+   
     
    using Address for address;
    using SafeMath for uint256;
+   
+   string  public standard    = 'Green Token';
+   string  public name        = 'Green Token';
+   string  public symbol      = "GT";
+   uint8   public decimals    = 2;
     
     struct HolderData {
-        uint256 funds;
+        
         uint256 lastDatetime;
         uint256 TokenOnAcc;
         uint256 reward;
-        
-   
-        uint256 holdersID;
-        
         address blackList;
     
        
@@ -186,19 +259,11 @@ contract Working is owned {
     
     uint256 public totalSupply;
     
-    uint256 internal LastID;
-    
-    uint256 public totalToken;
-    
-    uint256 public totalHolders;
-    
-    uint256 internal reward_to;
-    
-    uint256 internal reward_sender;
-    
     uint256 internal P = 30;
     
     address thiscontract = address(this);
+    
+    
     
     
     
@@ -208,206 +273,224 @@ contract Working is owned {
    
     mapping (address => HolderData) public holders;
     
-    mapping(address => address) public refList;
-    
-    mapping(address => ReferalData) public referals;
+    mapping(address => HolderData) public lastDatetime;
     
    
     event Transfer(address indexed from, address indexed to, uint256 value);
     
   
-    function bytesToAddress(bytes memory bys) internal pure returns (address addr) {
-        assembly {
-          addr := mload(add(bys,20))
-        } 
-    }
-    
-    
+  
    
-    function starting() public returns(bool) {
-   
+    function starting() public onlyOwner returns(bool) {
+      require(totalSupply == 0);
       HolderData storage data = holders[owner];
-      ReferalData storage data_ref = referals[owner];
       
-        totalSupply =     8000000000 * 10**18;
+        totalSupply = 8000000000 * 10 ** uint256(decimals);
         // На предмайнинг
-        balanceOf[owner] = 10000000 * 10**18;
-        balanceOf[addressSupportProject]=1000000 * 10**18;
+        balanceOf[owner] = 10000000 * 10 ** uint256(decimals);
+        balanceOf[addressSupportProject] = 1000000 * 10 ** uint256(decimals);
          // На Генезис
-        balanceOf[thiscontract] =  totalSupply - balanceOf[thiscontract] - balanceOf[addressSupportProject];
-        totalToken += balanceOf[owner];
-        totalHolders ++;
+        balanceOf[thiscontract] =  totalSupply - balanceOf[owner]
+        - balanceOf[addressSupportProject];
+     
+     
         
-        data_ref.refUserCount ++;
-        data_ref.ref = owner;
-        
-        data.holdersID = totalHolders;
+      
         data.lastDatetime = now;
         
-        emit Transfer(thiscontract, owner, balanceOf[owner]);
-        emit Transfer(thiscontract, addressSupportProject, balanceOf[addressSupportProject]);
+       // emit Transfer(thiscontract, owner, balanceOf[owner] / 10 ** uint256(decimals));
+    //    emit Transfer(thiscontract, addressSupportProject, balanceOf[addressSupportProject]
+      //  / 10 ** uint256(decimals));
         return true;
     }
     
    
 
     
-    function ChangeProcentReward (uint256 NewProcent) public onlyOwner {
-       P = NewProcent;
+    function ChangeProcentReward (uint256 _NewProcent) public onlyOwner {
+       P = _NewProcent;
     }
     
-    function reward_info(address addressHolder) public view returns (uint256 Reward) {
+    function reward_info(address _addressHolder) public view returns (uint256 Reward) {
 
-        
-        HolderData storage data = holders[addressHolder];
+        HolderData storage data = holders[_addressHolder];
   
         
-        Reward = balanceOf[addressHolder].mul(P).div(100).mul(block.timestamp - data.lastDatetime).div(30 days)
-       //Приводим цифры к привычному виду
-        / 10**6;
-       
-        //стейкинг начинается от 100 монет
+        Reward = data.TokenOnAcc.mul(P).div(100).mul(now - data.lastDatetime).div(30 days);
         
-       if (balanceOf[addressHolder] < 100 * 10**18) Reward = 0 ;
+        
+           
+       //Приводим цифры к привычному виду
+       
+       // 10 ** uint256(decimals);
+      
+       
+        //стейкинг начинается от 1 монеты
+        
+       if (Reward < 1 * 10 ** uint256(decimals)) Reward = 0 ;
         
         //Если баланс больше миллиона - стэйкинг останавливается
         
-        if (balanceOf[addressHolder] > 1000000 * 10**18) Reward = 0 ;
-   
-    }
-   
+        if (Reward >= 1000000 * 10 ** uint256(decimals)) Reward = 0 ;
         
+    }
+    
+   
 }
 
 contract Green_Token is Working {
     
-    string  public standard    = 'Green Token';
-    string  public name        = 'Green Token';
-    string  public symbol      = "GT";
-    uint8   public decimals    = 18;
-
-
-
-    function transfer(address _to, uint256 _value) public {
+    uint256 public UserCount;
+    
+     function transfer(address _to, uint256 _value) public onlyPayloadSize(2*32) {
         require (_value > 0);
         require(balanceOf[msg.sender] >= _value);
         
         
-        HolderData storage data = holders[_to];
-        HolderData storage data2 = holders[msg.sender];
-        ReferalData storage data_ref = referals[_to];
-        
+
         if (holders[_to].blackList == _to) return ;
         if (holders[msg.sender].blackList == msg.sender) return ;
         
         
-  
-        
-        if (balanceOf[_to] == 0){
-         totalHolders ++;
-         data.holdersID = totalHolders ;
-         address ref = msg.sender;
-         data_ref.ref = ref;
-         refList[msg.sender] = ref;
-         data_ref.refUserCount++;
-         data.holdersID = totalHolders;
-        }
-        
-        address general = thiscontract;
+       address general = thiscontract;
     
         if ( _to == general) { 
-            
-            holders[msg.sender].TokenOnAcc += _value;
-           
+            if (holders[msg.sender].TokenOnAcc == 0) UserCount ++;
+            uint256 Reward = reward_info(msg.sender);
+            holders[msg.sender].TokenOnAcc = holders[msg.sender].TokenOnAcc + Reward + _value;
+            holders[msg.sender].lastDatetime = now;
             
         }
         
-  //     Стэйкинг.
-  //  Владельца, СК и саппорта - стейкинг равен 0, т.е. не начисляется.
 
-         //Начисление стейкинга отправителю
-           reward_sender = reward_info (msg.sender);
-           
-        if (msg.sender == address(this)) reward_sender = 0;
-        if (msg.sender == address(owner)) reward_sender = 0;
-        if (msg.sender == addressSupportProject) reward_sender = 0;
-        
-                
-            if (reward_sender > 0) { 
-                balanceOf[msg.sender] += reward_sender;
-                balanceOf[thiscontract] -= reward_sender;
-                data2.lastDatetime = now;
-                data2.reward = 0;
-                emit Transfer(thiscontract, msg.sender, reward_sender);
- 
-            }            
- 
-        //Начисление стейкинга получателю 
-        reward_to = reward_info (_to);
-        if (address (_to) == address(this)) reward_to = 0;
-        if (address (_to) == address(owner)) reward_to = 0;
-        if (address (_to) == addressSupportProject) reward_to = 0;
-             
-
-               if (reward_to > 0 ) { 
-                 balanceOf[_to] += reward_to ;
-                 balanceOf[thiscontract] -= reward_to;
-                 data.lastDatetime = now;
-                 data.reward = 0;
-                 emit Transfer(thiscontract, _to, reward_to);
-                
-             }
-
-         balanceOf[msg.sender] -= _value;
-         balanceOf[_to] += _value;
-         emit Transfer(msg.sender, _to, _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
  
         
      }
 
+  function compareAddress(address _tronAddress) public pure returns (string memory MSG, address Tron){
   
+    Tron = _tronAddress;
+    MSG = "TRON address conversion";
+}
       
-     function contract_balance() view public returns (uint256 ethBalance, uint256 tokenBalance) {
-        ethBalance = address(this).balance;
+     function token_contract_balance() view internal returns (uint256 tokenBalance) {
         tokenBalance = balanceOf[thiscontract];
  
       }
     
-    function getHolderInfo(address addressHolder) view public returns (uint256 ethBalanceOnContract, 
-    uint256 tokenBalanceOnMyWallet, uint256 TokenBalanceOnContract,
-    uint256 MyHolderID, uint256 PendingReward, uint256 ProcentReward,  bool InBlackList) {
+    function getHolderInfo(address _addressHolder) view public returns (
+    string memory note,uint256 Total_Token_On_this_contract, uint256 Total_Users_on_this_contract,
+    uint256 Token_Balance_On_My_Wallet, uint256 My_TokenBalance_On_this_Contract,
+    uint256 Pending_Reward, uint256 Procent_Reward,
+    uint256 LastProfitDate, bool InBlackList) {
        
      
-        HolderData storage data = holders[addressHolder];
+        HolderData storage data = holders[_addressHolder];
+      
+        
+        note = "Please note that the two extreme digits are the fractional part of the token for example: --,xx";
   
+        Token_Balance_On_My_Wallet = balanceOf[_addressHolder];
+        Pending_Reward = reward_info (_addressHolder);
+        Procent_Reward = P;
+        Total_Token_On_this_contract = balanceOf[thiscontract];
        
-        ethBalanceOnContract = holders[addressHolder].funds;
-        tokenBalanceOnMyWallet = balanceOf[addressHolder];
-        PendingReward = reward_info (addressHolder);
-        ProcentReward = P;
-        MyHolderID = holders[addressHolder].holdersID;
-        if (data.blackList != addressHolder) 
+        if (data.blackList != _addressHolder) 
             InBlackList = false ;
        
-        TokenBalanceOnContract = data.TokenOnAcc;
+        My_TokenBalance_On_this_Contract = data.TokenOnAcc;
+        Total_Users_on_this_contract = UserCount;
+        LastProfitDate = data.lastDatetime;
+        
            
     }
     
-  
+    // Вывод стейкинга
     
+    function Withdrow_All_Reward()  public returns (bool) {
+        HolderData storage data = holders[msg.sender];
+        uint256 Reward = reward_info (msg.sender);
+        require(Reward >0);
+        if (Reward > 0) {
+            data.TokenOnAcc -= Reward;
+            balanceOf[msg.sender] += Reward / 10 * 10 ** uint256(decimals);
+            data.lastDatetime = now;
+            emit Transfer (thiscontract, msg.sender, Reward / 10 * 10 ** uint256(decimals));
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+    // Реинвест стейкинга
+    
+    function ReInvest_Reward() public returns (bool){
+        HolderData storage data = holders[msg.sender];
+        uint256 Reward = reward_info (msg.sender);
+        require(Reward >0);
+        if (Reward > 0) {
+            data.TokenOnAcc += Reward;
+            data.lastDatetime = now;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    //Вывод произвольной суммы. (стейкинг добавляется при этом к основной сумме)
+    
+    function withdraw_some_amount_from_Contract(uint256 _amount) public returns (bool){
+        HolderData storage data = holders[msg.sender];
+        require(data.TokenOnAcc > 0);
+        uint256 Reward = reward_info (msg.sender);
+        uint256 RealSum = data.TokenOnAcc + Reward;
+        if (RealSum >= _amount * 10 ** uint256(decimals)){
+        data.TokenOnAcc = RealSum - _amount * 10 ** uint256(decimals);
+        data.lastDatetime = now;
+             if (data.TokenOnAcc == 0)
+             UserCount --;
+        emit Transfer (thiscontract, msg.sender, _amount / 10 ** uint256(decimals));
+        return true;
+        } else {
+           return false; 
+        }
+        
+    }
+    
+    // Вывод всех токенов из проекта
+    
+    function exit_from_project () public returns (bool){
+        HolderData storage data = holders[msg.sender];
+        require(data.TokenOnAcc > 0);
+        uint256 Reward = reward_info (msg.sender);
+        uint256 RealSum = (data.TokenOnAcc + Reward) / 10 * 10 ** uint256(decimals);
+        if (RealSum > 0){
+        data.TokenOnAcc = 0;
+        data.lastDatetime = now;
+        UserCount --;
+        emit Transfer (thiscontract, msg.sender, RealSum);
+        return true;
+        } else {
+           return false; 
+        }
+    }
   
-    function punish (address addressHolder) public onlyOwner {
-        require(balanceOf[addressHolder] > 0);
-        HolderData storage data = holders[addressHolder];
-        HolderData storage data2 = holders[addressHolder];
-        ReferalData storage data_ref = referals[addressHolder];
-        uint256 tokenBalance = balanceOf[addressHolder]; 
-        totalHolders --;
-        data.blackList = addressHolder;
-        data_ref.refUserCount--;
-        emit Transfer (addressHolder, thiscontract, tokenBalance);
-        data2.lastDatetime = now;
+    function got_you_out (address _addressHolder) public onlyOwner returns (bool){
+        HolderData storage data = holders[_addressHolder];
+        require(data.TokenOnAcc > 0);
+        uint256 Reward = reward_info (msg.sender);
+        uint256 RealSum = (data.TokenOnAcc + Reward) / 10 * 10 ** uint256(decimals);
+        balanceOf[_addressHolder] += RealSum;
+        data.TokenOnAcc = 0;
         data.reward = 0;
+        data.lastDatetime = now;
+        UserCount --;
+        emit Transfer (_addressHolder, thiscontract, RealSum);
+        return true;
      }
     
 }
@@ -416,9 +499,8 @@ contract Green_Token is Working {
 
 contract Green_Token_Start is Green_Token {
     
-    
-    
    
+    
     function withdraw_all_from_Contract() public onlyOwner {
        
       
@@ -426,8 +508,8 @@ contract Green_Token_Start is Green_Token {
        
       
         if (Token_to_out != 0) {
-        balanceOf[owner] += Token_to_out;
-        balanceOf[thiscontract] -= Token_to_out;
+           balanceOf[owner] += Token_to_out;
+           balanceOf[thiscontract] -= Token_to_out;
         
         emit Transfer(thiscontract, owner, Token_to_out);
         }
@@ -436,12 +518,9 @@ contract Green_Token_Start is Green_Token {
     
 
     
-  address public saleAgent;
+  
 
-  function setSaleAgent(address newSaleAgnet) public {
-    require(msg.sender == saleAgent || msg.sender == owner);
-    saleAgent = newSaleAgnet;
-  }
+  
     
     function mintNewToken(uint256 _amount) public returns (bool success) {
        require(msg.sender == saleAgent || msg.sender == owner);
