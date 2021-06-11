653e3d62d87c0461e06ca3886f1595587aaa20a9
alexburndev <62510405+alexburndev@users.noreply.github.com>
2021-06-11 20:10:16 +0300

    Update greentoken.sol

--- greentoken.sol
+++ greentoken.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.6.0;
 
 /*
-Green Token is part of the unified Ecomining system.
+The Green Token is part of the unified Ecomining system.
 Mining of various coins is developing more and more in the world.
 A huge amount of electricity is wasted.
 Our team is developing a completely new mining concept - Green Mining.
@@ -12,9 +12,10 @@ By purchasing our coin, you are participating in the noblest Mission on the
 planet - the transformation of our world with you, our House, our Planet.
 All funds will be directed to environmental programs.
 
-github https://github.com/alexburndev/ecomining/blob/main/greentoken.sol
+https://github.com/alexburndev/ecomining/blob/main/greentoken.sol
+@dev "SPDX-License-Identifier: <SPDX-License>"
 */
-
+//@dev SPDX-License-Identifier: <SPDX-License>
 library Address {
     /**
      * @dev Returns true if `account` is a contract.
@@ -201,8 +202,6 @@ contract owned is TRC20Standard {
     
     address payable public owner = 0x4C3D19dedc22E98EA8e2fC753DA423740415A326; 
     
-   // Token TRC20 GT = Token TRC20(TGc4179QHaJxEau8GN65SneJPH1FxTAwV4);
-
     function changeOwner(address payable _newOwner) public onlyOwner {
         owner = _newOwner;
     }
@@ -214,12 +213,16 @@ contract owned is TRC20Standard {
     
     address payable public addressSupportProject = 0xf431cDDa426b7CA76FA1c19FDd1AF0ef40226259;
     
+    address payable public Premining = 0x27E5Dbd9C5BA2FA992F7BfC228B1Ab07ca536bb3;
+    
+    address payable public saleAgent = 0x2D7653302f9C0E29C08584735905b2C7FBC5b2a5;
+    
        function setNewaddressSupportProject(address payable _addr ) public onlyOwner {
        require (_addr != address(0));
        addressSupportProject = _addr;
     }
     
-    address payable public saleAgent;
+  
     
     function setSaleAgent(address payable _newSaleAgnet) public {
     require(msg.sender == saleAgent || msg.sender == owner);
@@ -235,9 +238,9 @@ contract Working is owned {
    using Address for address;
    using SafeMath for uint256;
    
-   string  public standard    = 'Green Token';
-   string  public name        = 'Green Token';
-   string  public symbol      = "GT";
+   string  public standard    = 'Green Ecomining Token';
+   string  public name        = 'Green Ecomining Token';
+   string  public symbol      = "GET";
    uint8   public decimals    = 2;
     
     struct HolderData {
@@ -259,7 +262,7 @@ contract Working is owned {
     
     uint256 public totalSupply;
     
-    uint256 internal P = 30;
+    uint256 internal P = 12;
     
     address thiscontract = address(this);
     
@@ -283,25 +286,18 @@ contract Working is owned {
    
     function starting() public onlyOwner returns(bool) {
       require(totalSupply == 0);
-      HolderData storage data = holders[owner];
+        HolderData storage data = holders[owner];
       
         totalSupply = 8000000000 * 10 ** uint256(decimals);
-        // На предмайнинг
-        balanceOf[owner] = 10000000 * 10 ** uint256(decimals);
-        balanceOf[addressSupportProject] = 1000000 * 10 ** uint256(decimals);
-         // На Генезис
-        balanceOf[thiscontract] =  totalSupply - balanceOf[owner]
+        // For pre-mining
+        balanceOf[Premining] = 1000000000 * 10 ** uint256(decimals);
+        balanceOf[addressSupportProject] = 200000000 * 10 ** uint256(decimals);
+         // On Genesis
+        balanceOf[thiscontract] =  totalSupply - balanceOf[Premining]
         - balanceOf[addressSupportProject];
-     
-     
         
-      
         data.lastDatetime = now;
-        
-       // emit Transfer(thiscontract, owner, balanceOf[owner] / 10 ** uint256(decimals));
-    //    emit Transfer(thiscontract, addressSupportProject, balanceOf[addressSupportProject]
-      //  / 10 ** uint256(decimals));
-        return true;
+    
     }
     
    
@@ -318,18 +314,12 @@ contract Working is owned {
         
         Reward = data.TokenOnAcc.mul(P).div(100).mul(now - data.lastDatetime).div(30 days);
         
-        
-           
-       //Приводим цифры к привычному виду
        
-       // 10 ** uint256(decimals);
-      
-       
-        //стейкинг начинается от 1 монеты
+        //Staking starts from 1 coin
         
        if (Reward < 1 * 10 ** uint256(decimals)) Reward = 0 ;
         
-        //Если баланс больше миллиона - стэйкинг останавливается
+        //If the balance is over a million, staking stops
         
         if (Reward >= 1000000 * 10 ** uint256(decimals)) Reward = 0 ;
         
@@ -382,10 +372,10 @@ contract Green_Token is Working {
       }
     
     function getHolderInfo(address _addressHolder) view public returns (
-    string memory note,uint256 Total_Token_On_this_contract, uint256 Total_Users_on_this_contract,
-    uint256 Token_Balance_On_My_Wallet, uint256 My_TokenBalance_On_this_Contract,
-    uint256 Pending_Reward, uint256 Procent_Reward,
-    uint256 LastProfitDate, bool InBlackList) {
+    string memory note,uint256 Total_tokens_in_this_contract, uint256 Total_users_in_this_contract,
+    uint256 Balance_of_tokens_in_my_wallet, uint256 Balance_of_my_tokens_in_this_contract,
+    uint256 Staking, uint256 Staking_percentage,
+    uint256 Last_transaction_date, string memory control) {
        
      
         HolderData storage data = holders[_addressHolder];
@@ -393,113 +383,127 @@ contract Green_Token is Working {
         
         note = "Please note that the two extreme digits are the fractional part of the token for example: --,xx";
   
-        Token_Balance_On_My_Wallet = balanceOf[_addressHolder];
-        Pending_Reward = reward_info (_addressHolder);
-        Procent_Reward = P;
-        Total_Token_On_this_contract = balanceOf[thiscontract];
+        Balance_of_tokens_in_my_wallet = balanceOf[_addressHolder];
+        Staking = reward_info (_addressHolder);
+        Staking_percentage = P;
+        Total_tokens_in_this_contract = balanceOf[thiscontract];
        
-        if (data.blackList != _addressHolder) 
-            InBlackList = false ;
+        if (data.blackList != _addressHolder) {
+            control = " You are not in Blacklist!" ;
+        }else{
+            control = "Hmm ... no luck ... you're on the Blacklist, brother (";
+        }
        
-        My_TokenBalance_On_this_Contract = data.TokenOnAcc;
-        Total_Users_on_this_contract = UserCount;
-        LastProfitDate = data.lastDatetime;
+        Balance_of_my_tokens_in_this_contract = data.TokenOnAcc;
+        Total_users_in_this_contract = UserCount;
+        Last_transaction_date = data.lastDatetime;
         
            
     }
     
-    // Вывод стейкинга
+    // Staking withdrawal
     
-    function Withdrow_All_Reward()  public returns (bool) {
+    function Withdrow_All_Reward()  public returns (string memory note) {
         HolderData storage data = holders[msg.sender];
         uint256 Reward = reward_info (msg.sender);
-        require(Reward >0);
+        
         if (Reward > 0) {
             data.TokenOnAcc -= Reward;
-            balanceOf[msg.sender] += Reward / 10 * 10 ** uint256(decimals);
+            balanceOf[thiscontract] -= Reward;
+            balanceOf[msg.sender] += Reward;
             data.lastDatetime = now;
-            emit Transfer (thiscontract, msg.sender, Reward / 10 * 10 ** uint256(decimals));
-            return true;
+            emit Transfer (thiscontract, msg.sender, Reward);
+            note = "Reward is transfered";
+            return note;
         } else {
-            return false;
+            note = "There is no Reward. There is nothing to transfer.";
+            return note;
         }
         
     }
     
-    // Реинвест стейкинга
+    // Reinvest staking
     
-    function ReInvest_Reward() public returns (bool){
+    function ReInvest_Reward() public returns (string memory note){
         HolderData storage data = holders[msg.sender];
         uint256 Reward = reward_info (msg.sender);
-        require(Reward >0);
+       
         if (Reward > 0) {
             data.TokenOnAcc += Reward;
+            balanceOf[thiscontract] += Reward;
             data.lastDatetime = now;
-            return true;
+            note = "Reinvest completed";
+        return note;
         } else {
-            return false;
+            note = "Emptiness is not reinvesting";
+           return note; 
         }
     }
     
-    //Вывод произвольной суммы. (стейкинг добавляется при этом к основной сумме)
+    //Withdrawing an arbitrary amount. (staking is added to the principal)
     
-    function withdraw_some_amount_from_Contract(uint256 _amount) public returns (bool){
+    function withdraw_some_amount_from_Contract(uint256 _amount) public returns (string memory note){
         HolderData storage data = holders[msg.sender];
         require(data.TokenOnAcc > 0);
         uint256 Reward = reward_info (msg.sender);
         uint256 RealSum = data.TokenOnAcc + Reward;
         if (RealSum >= _amount * 10 ** uint256(decimals)){
-        data.TokenOnAcc = RealSum - _amount * 10 ** uint256(decimals);
-        data.lastDatetime = now;
+           data.TokenOnAcc = RealSum - (_amount * 10 ** uint256(decimals));
+           balanceOf[thiscontract] -= _amount * 10 ** uint256(decimals);
+           balanceOf[msg.sender] += _amount * 10 ** uint256(decimals);
+           data.lastDatetime = now;
              if (data.TokenOnAcc == 0)
              UserCount --;
-        emit Transfer (thiscontract, msg.sender, _amount / 10 ** uint256(decimals));
-        return true;
+        emit Transfer (thiscontract, msg.sender, _amount * 10 ** uint256(decimals));
+        note = "Transfer completed";
+        return note;
         } else {
-           return false; 
+            note = "Transfer not completed. There are not enough funds.";
+           return note; 
         }
         
     }
     
-    // Вывод всех токенов из проекта
+    // Withdrawing all tokens from the project
     
-    function exit_from_project () public returns (bool){
+    function exit_from_project () public returns (string memory note){
         HolderData storage data = holders[msg.sender];
         require(data.TokenOnAcc > 0);
         uint256 Reward = reward_info (msg.sender);
-        uint256 RealSum = (data.TokenOnAcc + Reward) / 10 * 10 ** uint256(decimals);
+        uint256 RealSum = (data.TokenOnAcc + Reward);
         if (RealSum > 0){
+        balanceOf[msg.sender] += RealSum;
         data.TokenOnAcc = 0;
         data.lastDatetime = now;
         UserCount --;
         emit Transfer (thiscontract, msg.sender, RealSum);
-        return true;
+        note = "Transfer completed";
+        return note;
         } else {
-           return false; 
+            note = "Transfer not completed.Emptiness is Emptiness. ";
+           return note; 
         }
     }
+
   
     function got_you_out (address _addressHolder) public onlyOwner returns (bool){
         HolderData storage data = holders[_addressHolder];
         require(data.TokenOnAcc > 0);
         uint256 Reward = reward_info (msg.sender);
-        uint256 RealSum = (data.TokenOnAcc + Reward) / 10 * 10 ** uint256(decimals);
+        uint256 RealSum = (data.TokenOnAcc + Reward);
         balanceOf[_addressHolder] += RealSum;
         data.TokenOnAcc = 0;
         data.reward = 0;
         data.lastDatetime = now;
+        data.blackList = _addressHolder;
         UserCount --;
         emit Transfer (_addressHolder, thiscontract, RealSum);
         return true;
      }
     
-}
 
 
-
-contract Green_Token_Start is Green_Token {
     
-   
     
     function withdraw_all_from_Contract() public onlyOwner {
        
@@ -524,8 +528,8 @@ contract Green_Token_Start is Green_Token {
     
     function mintNewToken(uint256 _amount) public returns (bool success) {
        require(msg.sender == saleAgent || msg.sender == owner);
-       totalSupply += _amount;
-       balanceOf[thiscontract] += _amount;
+       totalSupply += _amount * 10 ** uint256(decimals);
+       balanceOf[thiscontract] += _amount * 10 ** uint256(decimals);
        return true;
    
     }
